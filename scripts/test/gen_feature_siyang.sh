ee4069f2c21e21616fe3d14bf1c2f0c0fcca6ec5
徐乾桐 <Winslow@xuqiantongdeMacBook-Pro.local>
2016-04-05 17:31:37 -0400

    feature v4

--- scripts/test/gen_feature_siyang.sh
+++ scripts/test/gen_feature_siyang.sh
@@ -2,7 +2,7 @@ python gen_feature.py \
     --gpu_id 0  \
     --img_dir /media/mmr6-home/lhy/Documents/Data/Vehicles/vehicles_with_plate/cropped_uncovered/ \
     --img_list /home/xqt/demo/retrieval_test/siyang.txt \
-    --feature_path /home/xqt/features/v2_siyang_fc7.fea \
-    --net_def /home/xqt/exp/deploy_multi_v2.prototxt\
-    --weights  /home/xqt/exp_res/v2-_iter_35454.caffemodel \
+    --feature_path /home/xqt/features/v4_siyang_fc7.fea \
+    --net_def /home/xqt/exp/deploy_multi_v4.prototxt\
+    --weights  /home/xqt/exp_res/v4_iter_514.caffemodel \
     --feat  fc7
--- scripts/test/gen_feature_wendeng.sh
+++ scripts/test/gen_feature_wendeng.sh
@@ -2,7 +2,7 @@ python gen_feature.py \
     --gpu_id 1  \
     --img_dir  /media/megatron-home/dwliang/data/wendeng_res/ \
     --img_list /home/xqt/demo/retrieval_test/wendeng.txt \
-    --feature_path /home/xqt/features/v2_wendeng_fc7.fea \
-    --net_def /home/xqt/exp/deploy_multi_v2.prototxt \
-    --weights /home/xqt/exp_res/v2-_iter_35454.caffemodel  \
+    --feature_path /home/xqt/features/v4_wendeng_fc7.fea \
+    --net_def /home/xqt/exp/deploy_multi_v4.prototxt \
+    --weights /home/xqt/exp_res/v4_iter_514.caffemodel  \
     --feat fc7
