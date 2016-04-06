8118e1231ec9481da0d65c79a705c292ed33ec6c
徐乾桐 <Winslow@xuqiantongdeMacBook-Pro.local>
2016-04-06 13:45:27 -0400

    v4 q3

--- scripts/test/brute_force_run.sh
+++ scripts/test/brute_force_run.sh
@@ -1,7 +1,7 @@
 python brute_force.py \
     --gpu_id 0 \
     --query_dir  /media/mmr6-home/lhy/Documents/Data/Vehicles/vehicles_with_plate/cropped_uncovered/ \
-    --query_list /home/xqt/demo/retrieval_test/query_all.txt \
+    --query_list /home/xqt/demo/retrieval_test/query_3.txt \
     --siyang_label /media/megatron-home/dwliang/search_eval/query/cropped_label/ \
     --nn_number 1000 \
     --net_def /home/xqt/exp/deploy_multi_v4.prototxt \
