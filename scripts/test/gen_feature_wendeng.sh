ebd557213e66bec8571b5935cfab4ff3bab4181d
徐乾桐 <Winslow@xuqiantongdeMacBook-Pro.local>
2016-04-12 21:33:09 -0400

    gen _v4

--- scripts/test/gen_feature_wendeng.sh
+++ scripts/test/gen_feature_wendeng.sh
@@ -2,7 +2,7 @@ python gen_feature.py \
     --gpu_id 1  \
     --img_dir  /media/megatron-home/dwliang/data/wendeng_res/ \
     --img_list /home/xqt/demo/retrieval_test/wendeng.txt \
-    --feature_path /home/xqt/features/v4_wendeng_fc7.fea \
+    --feature_path /home/xqt/features/_v4_wendeng_fc7.fea \
     --net_def /home/xqt/exp/deploy_multi_v4.prototxt \
-    --weights /home/xqt/exp_res/v4_iter_514.caffemodel  \
+    --weights /home/xqt/exp_res/_v4_iter_14897.caffemodel  \
     --feat fc7
